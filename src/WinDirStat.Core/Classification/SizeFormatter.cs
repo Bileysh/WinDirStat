0f169c28575bef20ea6dc73f4344d0ab051ef7f1
Bileysh <bileysh06@knu.ua>
2026-08-05 18:09:42 +0300

    Implement NodeViewModel and bind file system tree to UI

---
+++ src/WinDirStat.Core/Classification/SizeFormatter.cs
@@ -0,0 +1,12 @@
+﻿namespace WinDirStat.Core.Classification;
+
+public static  class SizeFormatter
+{
+    public static string Format(long bytes) => bytes switch
+    {
+        >= 1_073_741_824 => $"{bytes / 1_073_741_824.0:F2} GB",
+        >= 1_048_576 => $"{bytes / 1_048_576.0:F2} MB",
+        >= 1024 => $"{bytes / 1024.0:F1} KB",
+        _ => $"{bytes} B"
+    };
+}
