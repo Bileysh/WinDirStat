e260bcd1db21c056d376ed983d390a11d6009e38
Bileysh <bileysh06@knu.ua>
2026-08-21 11:28:51 +0300

    Elevated folder picker

--- src/WinDirStat.App/Services/FolderPickerService.cs
+++ src/WinDirStat.App/Services/FolderPickerService.cs
@@ -1,15 +1,27 @@
 ﻿using Windows.Storage.Pickers;
 using WinDirStat.Core.Interfaces;
+using WinDirStat.Services;
 using WinRT.Interop;
 
 namespace WinDirStat_App.Services;
 
-public class FolderPickerService: IFolderPickerService
+public class FolderPickerService : IFolderPickerService
 {
-    public async Task<string?> PickFolderAsync()
+    public Task<string?> PickFolderAsync()
+    {
+        var hwnd = WindowNative.GetWindowHandle(App.MainWindow);
+
+        if (ElevationHelper.IsElevated())
+        {
+            return Task.FromResult(ClassicFolderPicker.Show(hwnd, "Оберіть папку для сканування"));
+        }
+
+        return PickFolderNormalAsync(hwnd);
+    }
+
+    private static async Task<string?> PickFolderNormalAsync(IntPtr hwnd)
     {
         var picker = new FolderPicker();
-        var hwnd = WindowNative.GetWindowHandle(App.MainWindow); 
         InitializeWithWindow.Initialize(picker, hwnd);
         picker.FileTypeFilter.Add("*");
 
