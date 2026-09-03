d4f61096b4905efc447adec95d67a72513468843
Bileysh <bileysh06@knu.ua>
2026-09-03 16:48:07 +0300

    Optimize C: drive scan performance

--- src/WinDirStat.Core/Interfaces/IDiskScanService.cs
+++ src/WinDirStat.Core/Interfaces/IDiskScanService.cs
@@ -5,5 +5,5 @@ namespace WinDirStat.Core.Interfaces;
 public interface IDiskScanService
 {
     Task<ScanResult> ScanAsync(string rootPath, CancellationToken cancellationToken = default,
-        bool useElevatedFallbackForAccessDenied = false, IProgress<ScanProgress>? progress = null);
+        bool useElevatedFallbackForAccessDenied = false, IProgress<ScanProgress>? progress = null, bool accountForHardLinks = false);
 }
