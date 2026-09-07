07e514919346ce8569d6eb7edac7b7b6945c3843
Bileysh <bileysh06@knu.ua>
2026-09-07 16:43:29 +0300

    Implement single instance, rich activation, and scan export/import

--- src/WinDirStat.ViewModels/MainPageViewModel.cs
+++ src/WinDirStat.ViewModels/MainPageViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
+using System.IO;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using WinDirStat.Core.Classification;
